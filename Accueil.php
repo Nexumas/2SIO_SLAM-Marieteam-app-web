ce38dca570e5721abb4be913eda06d63f77ae09d
theo-blampain <71644167+theo-blampain@users.noreply.github.com>
2020-10-12 16:10:58 +0200

    2020/10/12 commit 1

---
+++ Accueil.php
@@ -0,0 +1,44 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<title>MarieTeam - Accueil</title>
+<link rel="stylesheet" href="css/bootstrap.css">
+
+</head>
+
+<meta charset="utf-8" >
+<meta name="Auteurs"
+    content="Tharbla
+             Twolf59" >
+
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+  <a class="navbar-brand" href="#">Navbar w/ text</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+    <form class="form-inline">
+    <button class="btn btn-sm btn-outline-secondary" type="button"><a class="nav-link" href="inscription.php">Inscription</a></button>
+    </form>
+  </div>
+</nav>
+<h1>Yolo</h1>
+</body>
+</html>
