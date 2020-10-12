3366249a9d6d4f5d38c3c724b41c3fcb91d614b9
Twolf59 <tomdsx.59118@gmail.com>
2020-10-12 17:20:31 +0200

    XC

--- inscription.php
+++ inscription.php
@@ -20,11 +20,8 @@
       <div class="col-md-6 mx-auto text-center">
          <div class="header-title">
             <h1 class="wv-heading--title">
-               Check out — it’s free!
+               Inscription
             </h1>
-            <h2 class="wv-heading--subtitle">
-               Lorem ipsum dolor sit amet! Neque porro quisquam est qui do dolor amet, adipisci velit...
-            </h2>
          </div>
       </div>
       <div class="row">
@@ -41,21 +38,9 @@
                      <input type="number" min="0" name="phone" id="phone"  class="form-control my-input" placeholder="Phone">
                   </div>
                   <div class="text-center ">
-                     <button type="submit" class=" btn btn-block send-button tx-tfm">Create Your Free Account</button>
+                     <button type="submit" class=" btn btn-block send-button tx-tfm">S'inscrire</button>
                   </div>
-                  <div class="col-md-12 ">
-                     <div class="login-or">
-                        <hr class="hr-or">
-                        <span class="span-or">or</span>
-                     </div>
-                  </div>
-                  <div class="form-group">
-                     <a class="btn btn-block g-button" href="#">
-                     <i class="fa fa-google"></i> Sign up with Google
-                     </a>
-                  </div>
-                  <p class="small mt-3">By signing up, you are indicating that you have read and agree to the <a href="#" class="ps-hero__content__link">Terms of Use</a> and <a href="#">Privacy Policy</a>.
-                  </p>
+            
                </form>
             </div>
          </div>
