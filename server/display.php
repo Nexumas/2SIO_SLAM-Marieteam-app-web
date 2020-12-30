866cdd796b3c91736dd21914f5b6f680dd3f70ee
theo-blampain <71644167+theo-blampain@users.noreply.github.com>
2020-12-30 16:54:32 +0100

    Commit 2020-12-30 5
    fonctions stats fonctionnelles

--- server/display.php
+++ server/display.php
@@ -16,9 +16,11 @@ if(isset($_POST['periode'])){
     $dateDebut = date ("Y/m/d", strtotime($date1)); //change le format de la date
     $date2 = $_POST['dateFin'];
     $dateFin = date ("Y/m/d", strtotime($date2)); //change le format de la date
+
+    //Pour le CA
     $resCA = 0;
 
-    $reqCA = $conn->prepare("SELECT SUM(R.prixTotal) AS CA FROM reservation as R, traverse AS T WHERE R.idTraverse = T.idTraverse AND R.dateReservation BETWEEN ? AND ?"); //sélectionne la somme de prixTotal des réservations des traversées dans la période donnée
+    $reqCA = $conn->prepare("SELECT SUM(R.prixTotal) AS CA FROM reservation as R, traverse AS T WHERE R.idTraverse = T.idTraverse AND T.dateDepart BETWEEN ? AND ?"); //sélectionne la somme de prixTotal des réservations des traversées dans la période donnée
     $reqCA->bind_param("ss",$dateDebut,$dateFin);
     $reqCA->execute();
     $value = $reqCA->get_result();
@@ -29,6 +31,34 @@ if(isset($_POST['periode'])){
 
     $_SESSION['CA'] = $resCA; //pour le transport du chiffre d'affaire dans stats.php
     echo $resCA; //test d'affichage du résultat de la requête
+    echo '||';
+
+
+    //Pour les passagers
+    $resPassager = array(); //pour stocker la catégorie et le nombre de passagers de celle-ci
+    $resTotal = 0; //pour stocker le total de passagers
+
+    $reqPassager = $conn->prepare("SELECT Ty.libelle AS libelle, SUM(A.nombrePlaces) AS Passagers FROM associer AS A, reservation AS R, traverse AS Tr, typeplace as Ty WHERE A.idReservation = R.idReservation AND R.idTraverse = Tr.idTraverse AND A.idType = Ty.idType AND Tr.dateDepart BETWEEN ? AND ? GROUP BY Ty.libelle"); //renvoit le total de passager par catégorie dans la période donnée
+    $reqPassager->bind_param("ss",$dateDebut,$dateFin);
+    $reqPassager->execute();
+    $value2 = $reqPassager->get_result();
+
+    while($data2 = $value2->fetch_assoc()){
+        array_push($resPassager, array($data2['libelle'], $data2['Passagers'])); //stocke le nombre de passagers par catégorie
+    }
+
+    $i = 0;
+    while($i<count($resPassager)){
+        $resTotal = $resTotal + $resPassager[$i][1]; //calcule le total de passagers
+        echo $resPassager[$i][0].' = '.$resPassager[$i][1]." | "; //test du résultat de la requête SQL
+        $i = $i + 1;
+    }
+
+    echo '||';
+    echo $resTotal; //test de la boucle ci-dessus
+
+    $_SESSION['Cat'] = $resPassager; //transport du nombre de passagers par catégorie vers stats.php
+    $_SESSION['Total'] = $resTotal; //transport du total de passagers vers stats.php
 }
 
 ?>
